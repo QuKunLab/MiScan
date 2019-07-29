c2d1d25b3254c3bd6c23caa686efe7597eb542cc
jeffery <jeffery_cpu@163.com>
2019-07-29 22:01:08 +0800

    add code comment

--- MiScan/utils/util.py
+++ MiScan/utils/util.py
@@ -4,6 +4,12 @@ HERE = Path(__file__).parent.parent
 
 
 def time_it(func):
+    """
+    time record decorator
+
+    :param func:
+    :return: wrapper function, callable
+    """
     def wrapper(*args, **kwargs):
         start = time()
         func(*args, **kwargs)
@@ -12,6 +18,11 @@ def time_it(func):
 
 
 def get_data():
+    """
+    get dependency data config
+
+    :return: dict
+    """
     return {
         'inFeaBed': HERE / 'dependency_data/tcga_13885fea_exon_cut_100bp.bed',
         'inFeaID': HERE / 'dependency_data/13885fea_exon_cut_100bp_2sample.txt',
