e2e6b417fadf8823d5ceda0432ad9110493ddb8c
jeffery <44384930+jefferyUstc@users.noreply.github.com>
2019-08-29 18:53:19 +0800

    update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="MiScan",
-    version="1.0.1",
+    version="1.0.1.post1",
     author='Qulab USTC',
     author_email="jeffery_cpu@163.com",
     description="Maxout-inferred SNV-based cancer prediction model",
